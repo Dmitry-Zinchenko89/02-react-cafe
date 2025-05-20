c277d81e3fb7e93f0928db64a381e120296a82a7
Dmitry <zinchenko89d@gmail.com>
2025-05-20 10:50:45 +0300

    fixed Notification

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,10 +1,8 @@
+import React from 'react';
 import css from './Notification.module.css';
 
-interface NotificationProps {
-  message: string;
-}
-const Notification = ({ message }: NotificationProps) => {
-  return <p className={css.message}>{message}</p>;
+const Notification: React.FC = () => {
+  return <p className={css.message}>No feedback yet</p>;
 };
 
 export default Notification;
